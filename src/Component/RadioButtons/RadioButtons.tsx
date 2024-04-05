c15f91da15b79819485f4171e9d55d0e59e9dcd0
biggieliten <filipisaksson00@gmail.com>
2024-04-05 09:43:58 +0200

    .

---
+++ src/Component/RadioButtons/RadioButtons.tsx
@@ -0,0 +1,16 @@
+const RadioButtons = () => {
+  return (
+    <>
+      <form>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" name="" id="" />
+          <label htmlFor=""></label>
+          <input type="text" name="" id="" />
+          <label htmlFor=""></label>
+          <input type="text" name="" id="" />
+        </div>
+      </form>
+    </>
+  );
+};
